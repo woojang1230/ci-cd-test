4b3b7aad092200d09c2f45a0a2851a137b61000f
woojang1230 <woojang@kumsung.co.kr>
2021-11-01 16:53:40 +0900

    init project

---
+++ src/main/java/kr/co/kumsung/cicdtest/CiCdTestApplication.java
@@ -0,0 +1,13 @@
+package kr.co.kumsung.cicdtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiCdTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiCdTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/kumsung/cicdtest/CiCdTestApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.kumsung.cicdtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiCdTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
